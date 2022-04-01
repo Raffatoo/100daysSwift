de832bd2146cda39c63694ade464230a4140cbb3
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2022-04-01 06:37:32 -0600

    var and let
    Variables, simple data types, and string interpolation

---
+++ 001day/variables.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+// 100 Days of Swift
+
+// Variables, simple data types, string interpolation
+
+// una variables tiene la propiedad que puede cambia de valor.
+
+// definiedo una variable con el nombre str
+
+var str = "Hello, playground"
+
+str = "Cambiando el valor del conttenido"
+
+// el tipo de dato puede ser definido
+
+// definiendo el tipo de dato implícito
+
+var nombre = "Hector"
+
+// definiendo el tipo de dato de maera explícito
+
+var eddad: Int = 60
+
+// creando una variable de tipo String multilinea
+
+var textoMmultiliinea = """
+    Hola aqui estamos
+    entusiasmados por aprender
+    no hay conociento malo
+    """
+
+// Variables Doubles
+
+var pi = 3.1416
+var phi: Double = 3.1416
+
+
+// Variables booleanas
+
+var circuloVerde = false
+var esCirculoVerde: Bool = false
+
+// String interpolacion, para incluir valor de variables/Constantes
+
+var oracion = " es \(esCirculoVerde) que el circulo sea Verde"
+
+// las conatntes son definidas por la pallabra reservada let
+
+let taylor = "Swift"
+
+// pueden tener una definicion de variable implícita o explícita
+
+let album: String = "Iniciando Primavera"
+
+let year : Int = 1910
+
+let muroBlanco: Bool = false
+
